931362485adeeb39733b5910b2bd492f8517bace
Repec <gabriela.repec@nokia.com>
2020-08-24 15:42:10 +0200

    teambonus calculator) :q

--- src/main/java/tdd/bonus/TeamBonusCalculator.java
+++ src/main/java/tdd/bonus/TeamBonusCalculator.java
@@ -2,7 +2,27 @@ package tdd.bonus;
 
 public class TeamBonusCalculator {
 
-	public double calculate(final int sales) {
-		return -1;
+	private final int quota;
+	private int teamMembersNumber;
+	private int commissionPercentage;
+
+	public TeamBonusCalculator(final int quota){
+		this.quota = quota;
 	}
+
+	public TeamBonusCalculator(final int quota,final int teamMembersNumber, final int commissionPercentage){
+		this.quota = quota;
+		this.teamMembersNumber = teamMembersNumber;
+		this.commissionPercentage = commissionPercentage;
+	}
+
+	public double calculate(final int sales) {
+		if(sales<=quota || teamMembersNumber==0) return 0;
+		else return ((sales-quota)*getCommissionPercentageValue())/teamMembersNumber;
+	}
+
+	private double getCommissionPercentageValue() {
+		return (double) commissionPercentage/100;
+	}
+
 }
--- src/test/java/tdd/bonus/TeamBonusCalculatorTest.java
+++ src/test/java/tdd/bonus/TeamBonusCalculatorTest.java
@@ -9,8 +9,33 @@ public class TeamBonusCalculatorTest {
 
     @Test
     public void calculate_shouldReturnZero_whenSalesIsSmallerThanQuota() {
-        TeamBonusCalculator teamBonusCalculator=new TeamBonusCalculator();
+        TeamBonusCalculator teamBonusCalculator=new TeamBonusCalculator(1000);
+        assertThat(teamBonusCalculator.calculate(900)).isEqualTo(0);
+    }
+
+    @Test
+    public void calculate_shouldReturnZero_whenSalesEqualsQuota() {
+        TeamBonusCalculator teamBonusCalculator=new TeamBonusCalculator(1000);
         assertThat(teamBonusCalculator.calculate(1000)).isEqualTo(0);
+    }
+
+
+    @Test
+    public void calculate_shouldReturnValue_whenSalesMoreThanQuota() {
+        TeamBonusCalculator teamBonusCalculator=new TeamBonusCalculator(11000,4,10);
+        assertThat(teamBonusCalculator.calculate(12000)).isEqualTo(25);
+    }
+
+    @Test
+    public void calculate_shouldReturn100_whenSalesMoreThanQuota() {
+        TeamBonusCalculator teamBonusCalculator=new TeamBonusCalculator(11000,5,50);
+        assertThat(teamBonusCalculator.calculate(12000)).isEqualTo(100);
+    }
+
+    @Test
+    public void calculate_shouldReturnZero_whenTeamMembersIsZero() {
+        TeamBonusCalculator teamBonusCalculator=new TeamBonusCalculator(11000,0,10);
+        assertThat(teamBonusCalculator.calculate(12000)).isEqualTo(0);
     }
 
 }
